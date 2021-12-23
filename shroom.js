fde6d6c07fc54b2914bd53c7d0335ce98f2250da
dr.biscuits <drbiscuits.eth@gmail.com>
2021-12-23 12:42:18 -0500

    only showing unique users

--- shroom.js
+++ shroom.js
@@ -3,17 +3,11 @@
 const web3 = require("@solana/web3.js");
 const connection = new web3.Connection("https://api.mainnet-beta.solana.com");
 const anchor = require('@project-serum/anchor')
-
 const idl = JSON.parse(require('fs').readFileSync('idl.json', 'utf8'));
-
 const metaplex = require("@metaplex/js");
 const {metadata: { MetadataData }} = metaplex.programs;
-
-
 const METAPLEX_PROGRAM_ID = new anchor.web3.PublicKey("metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s");
 let programId = new anchor.web3.PublicKey("GGYjKF1LBpqL37qcSetisYj8yQJf4kdQgoGPW7Po6P56");
-
-
 const key = anchor.web3.Keypair.generate();
 const opts = {
   preflightCommitment: "processed"
@@ -47,9 +41,10 @@ async function printStakedUsers() {
   accounts = accounts.filter(a => a.account.timestamp > 0)
 
   let metadatas = await getMetadata(accounts);
-  metadatas = metadatas.filter(a => a[1].data.symbol === symbol.toString())
-  console.log("Users staking", symbol, ":", metadatas.length);
-  metadatas.forEach(m => console.log(m[0].account.userKey.toString()));
+  users = metadatas.filter(a => a[1].data.symbol === symbol.toString()).map(a => a[0].account.userKey.toString());
+  let uniqueUsers = [...new Set(users)];
+  console.log("Users staking", symbol, ":", uniqueUsers.length);
+  uniqueUsers.forEach(u => console.log(u));
 }
 
 
